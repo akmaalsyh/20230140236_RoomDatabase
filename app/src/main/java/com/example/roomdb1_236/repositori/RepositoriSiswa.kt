62a4c44cb6b8e373a176f4fc31e3c47f57a543db
akmaalsyh <taufansyah50@gmail.com>
2025-12-04 11:53:21 +0700

    mengubah parameter getSiswaStream

--- app/src/main/java/com/example/roomdb1_236/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/roomdb1_236/repositori/RepositoriSiswa.kt
@@ -7,9 +7,7 @@ import kotlinx.coroutines.flow.Flow
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
     suspend fun insertSiswa(siswa: Siswa)
-
-    // Edit 1 : tambah 3 fungsi berikut
-    fun getSiswaStream(idSiswa: Int): Flow<Siswa?>
+    fun getSiswaStream(id: Int): Flow<Siswa?>
     suspend fun deleteSiswa(siswa: Siswa)
     suspend fun updateSiswa(siswa: Siswa)
 }
@@ -19,9 +17,7 @@ class OfflineRepositoriSiswa(
 ) : RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
-
-    // Edit 2 : tambah 3 override berikut
-    override fun getSiswaStream(idSiswa: Int): Flow<Siswa?> = siswaDao.getSiswa(idSiswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
     override suspend fun updateSiswa(siswa: Siswa) = siswaDao.update(siswa)
 }
