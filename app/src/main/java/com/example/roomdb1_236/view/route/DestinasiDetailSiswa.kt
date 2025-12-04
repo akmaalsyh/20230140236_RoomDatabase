1a7e2b4343cc4288402071222d9b9174cdb93f4b
akmaalsyh <taufansyah50@gmail.com>
2025-12-04 15:12:40 +0700

    Memperbarui DestinasiDetailSiswa

--- app/src/main/java/com/example/roomdb1_236/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/roomdb1_236/view/route/DestinasiDetailSiswa.kt
@@ -2,9 +2,13 @@ package com.example.roomdb1_236.view.route
 
 import com.example.roomdb1_236.R
 
-object DestinasiDetailSiswa : DestinasiNavigasi {
-    override val route = "detail_siswa"
-    override val titleRes = R.string.detail_siswa
-    const val itemIdArg = "idSiswa"
+object DestinasiDetailSiswa {
+    const val route = "detail_siswa"
+    const val itemIdArg = "itemId"
+
     val routeWithArgs = "$route/{$itemIdArg}"
+
+    val titleRes = R.string.detail_siswa
+
+    fun createRoute(itemId: Int): String = "$route/$itemId"
 }
