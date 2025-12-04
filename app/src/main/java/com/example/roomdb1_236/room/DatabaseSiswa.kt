ff05d94b6a2f70c8b0eeada8160ece7393e18cd5
akmaalsyh <taufansyah50@gmail.com>
2025-12-04 15:12:32 +0700

    mengimplementasikan database siswa dan dao

--- app/src/main/java/com/example/roomdb1_236/room/DatabaseSiswa.kt
+++ app/src/main/java/com/example/roomdb1_236/room/DatabaseSiswa.kt
@@ -6,20 +6,20 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 
 @Database(entities = [Siswa::class], version = 1, exportSchema = false)
-abstract class DatabaseSiswa : RoomDatabase() {
-
+abstract class DatabaseSiswa : RoomDatabase(){
     abstract fun siswaDao(): SiswaDao
 
-    companion object {
+    companion object{
         @Volatile
-        private var Instance: DatabaseSiswa? = null
+        private var instance: DatabaseSiswa? = null
 
         fun getDatabase(context: Context): DatabaseSiswa {
-            return (Instance ?: synchronized(this) {
+            return(instance?: synchronized  (lock = this){
                 Room.databaseBuilder(
-                    context, DatabaseSiswa::class.java,
-                    "siswa_database")
-                    .build().also { Instance = it }
+                    context, klass= DatabaseSiswa::class.java,
+                    name= "siswa.database")
+                    .build().also { instance= it }
+
             })
         }
     }
